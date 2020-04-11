7cdeea11198cd939f4455a2c66d6ae35fc04a65e
Administrator <admin@parabay.com>
2020-04-11 08:33:38 -0400

    Doctor patients list updated

--- app/src/main/java/com/booleanrhapsody/kram/adapter/DoctorsPatientsActivityMessagesRecyclerViewAdapter.java
+++ app/src/main/java/com/booleanrhapsody/kram/adapter/DoctorsPatientsActivityMessagesRecyclerViewAdapter.java
@@ -8,151 +8,83 @@
 
 package com.booleanrhapsody.kram.adapter;
 
-import android.databinding.DataBindingUtil;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.booleanrhapsody.kram.R;
-import com.booleanrhapsody.kram.databinding.MessageEighteenViewHolderBinding;
-import com.booleanrhapsody.kram.databinding.MessageFifteenViewHolderBinding;
-import com.booleanrhapsody.kram.databinding.MessageFourteenViewHolderBinding;
-import com.booleanrhapsody.kram.databinding.MessageSeventeenViewHolderBinding;
-import com.booleanrhapsody.kram.databinding.MessageSixteenViewHolderBinding;
 import com.booleanrhapsody.kram.databinding.MessageThirteenViewHolderBinding;
-import java.util.*;
+import com.booleanrhapsody.kram.model.PatientModel;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
 
 
-public class DoctorsPatientsActivityMessagesRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-	public static final int MESSAGE_THIRTEEN_VIEW_HOLDER_VIEW_TYPE = 1;
-	public static final int MESSAGE_FOURTEEN_VIEW_HOLDER_VIEW_TYPE = 2;
-	public static final int MESSAGE_FIFTEEN_VIEW_HOLDER_VIEW_TYPE = 3;
-	public static final int MESSAGE_SIXTEEN_VIEW_HOLDER_VIEW_TYPE = 4;
-	public static final int MESSAGE_SEVENTEEN_VIEW_HOLDER_VIEW_TYPE = 5;
-	public static final int MESSAGE_EIGHTEEN_VIEW_HOLDER_VIEW_TYPE = 6;
-	
-	public static final List<Integer> MOCK_DATA = Arrays.asList(MESSAGE_THIRTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_FOURTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_FIFTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_SIXTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_SEVENTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_EIGHTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_THIRTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_FOURTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_FIFTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_SIXTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_SEVENTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_EIGHTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_THIRTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_FOURTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_FIFTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_SIXTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_SEVENTEEN_VIEW_HOLDER_VIEW_TYPE, MESSAGE_EIGHTEEN_VIEW_HOLDER_VIEW_TYPE);
-	
-	@Override
-	public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-	
-		switch (viewType) {
-			case MESSAGE_THIRTEEN_VIEW_HOLDER_VIEW_TYPE: 
-				return new MessageThirteenViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_thirteen_view_holder, parent, false));
-			case MESSAGE_FOURTEEN_VIEW_HOLDER_VIEW_TYPE: 
-				return new MessageFourteenViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_fourteen_view_holder, parent, false));
-			case MESSAGE_FIFTEEN_VIEW_HOLDER_VIEW_TYPE: 
-				return new MessageFifteenViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_fifteen_view_holder, parent, false));
-			case MESSAGE_SIXTEEN_VIEW_HOLDER_VIEW_TYPE: 
-				return new MessageSixteenViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_sixteen_view_holder, parent, false));
-			case MESSAGE_SEVENTEEN_VIEW_HOLDER_VIEW_TYPE: 
-				return new MessageSeventeenViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_seventeen_view_holder, parent, false));
-			case MESSAGE_EIGHTEEN_VIEW_HOLDER_VIEW_TYPE: 
-				return new MessageEighteenViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_eighteen_view_holder, parent, false));
-		}
-		
-		throw new RuntimeException("Unsupported view type");
+public class DoctorsPatientsActivityMessagesRecyclerViewAdapter extends
+		FirestoreAdapter<DoctorsPatientsActivityMessagesRecyclerViewAdapter.MessageViewHolder>  {
+
+	public interface OnPatientSelectedListener {
+
+		void onPatientSelected(DocumentSnapshot restaurant);
+
 	}
-	
-	@Override
-	public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
-	
-		// Here you can bind RecyclerView item data.
+
+	private DoctorsPatientsActivityMessagesRecyclerViewAdapter.OnPatientSelectedListener mListener;
+
+	public DoctorsPatientsActivityMessagesRecyclerViewAdapter(Query query) {
+		super(query);
 	}
-	
-	@Override
-	public int getItemViewType(int position) {
-	
-		return MOCK_DATA.get(position);
+
+	public DoctorsPatientsActivityMessagesRecyclerViewAdapter(Query query, DoctorsPatientsActivityMessagesRecyclerViewAdapter.OnPatientSelectedListener listener) {
+		super(query);
+		mListener = listener;
 	}
-	
+
+	@NonNull
 	@Override
-	public int getItemCount() {
-	
-		return MOCK_DATA.size();
+	public DoctorsPatientsActivityMessagesRecyclerViewAdapter.MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+		return new DoctorsPatientsActivityMessagesRecyclerViewAdapter.MessageViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_thirteen_view_holder, parent, false));
 	}
-	
-	
-	public static class MessageThirteenViewHolder extends RecyclerView.ViewHolder {
+
+	@Override
+	public void onBindViewHolder(@NonNull DoctorsPatientsActivityMessagesRecyclerViewAdapter.MessageViewHolder messageViewHolder, int position) {
+		messageViewHolder.bind(getSnapshot(position), mListener);
+	}
+
+
+	public static class MessageViewHolder extends RecyclerView.ViewHolder {
 		private MessageThirteenViewHolderBinding binding;
-		public MessageThirteenViewHolder(View itemView) {
+		public MessageViewHolder(View itemView) {
 			super(itemView);
 			binding = MessageThirteenViewHolderBinding.bind(itemView);
 			init();
 		}
-		
+
 		public void init() {
-		
+
+		}
+
+		public void bind(final DocumentSnapshot snapshot,
+						 final DoctorsPatientsActivityMessagesRecyclerViewAdapter.OnPatientSelectedListener listener) {
+
+			PatientModel patient = snapshot.toObject(PatientModel.class);
+			Resources resources = itemView.getResources();
+
+
+			binding.nameTextView.setText(patient.getName());
+			binding.theNeedsOfTheFewTextView.setText(String.valueOf(patient.getSeverity()));
+
+			itemView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					if (listener != null) {
+						listener.onPatientSelected(snapshot);
+					}
+				}
+			});
 		}
 	}
-	
-	
-	public static class MessageFourteenViewHolder extends RecyclerView.ViewHolder {
-		private MessageFourteenViewHolderBinding binding;
-		public MessageFourteenViewHolder(View itemView) {
-			super(itemView);
-			binding = MessageFourteenViewHolderBinding.bind(itemView);
-			init();
-		}
-		
-		public void init() {
-		
-		}
-	}
-	
-	
-	public static class MessageFifteenViewHolder extends RecyclerView.ViewHolder {
-		private MessageFifteenViewHolderBinding binding;
-		public MessageFifteenViewHolder(View itemView) {
-			super(itemView);
-			binding = MessageFifteenViewHolderBinding.bind(itemView);
-			init();
-		}
-		
-		public void init() {
-		
-		}
-	}
-	
-	
-	public static class MessageSixteenViewHolder extends RecyclerView.ViewHolder {
-		private MessageSixteenViewHolderBinding binding;
-		public MessageSixteenViewHolder(View itemView) {
-			super(itemView);
-			binding = MessageSixteenViewHolderBinding.bind(itemView);
-			init();
-		}
-		
-		public void init() {
-		
-		}
-	}
-	
-	
-	public static class MessageSeventeenViewHolder extends RecyclerView.ViewHolder {
-		private MessageSeventeenViewHolderBinding binding;
-		public MessageSeventeenViewHolder(View itemView) {
-			super(itemView);
-			binding = MessageSeventeenViewHolderBinding.bind(itemView);
-			init();
-		}
-		
-		public void init() {
-		
-		}
-	}
-	
-	
-	public static class MessageEighteenViewHolder extends RecyclerView.ViewHolder {
-		private MessageEighteenViewHolderBinding binding;
-		public MessageEighteenViewHolder(View itemView) {
-			super(itemView);
-			binding = MessageEighteenViewHolderBinding.bind(itemView);
-			init();
-		}
-		
-		public void init() {
-		
-		}
-	}
+
 }
--- app/src/main/java/com/booleanrhapsody/kram/fragment/DoctorsPatientsActivity.java
+++ app/src/main/java/com/booleanrhapsody/kram/fragment/DoctorsPatientsActivity.java
@@ -20,11 +20,15 @@ import com.booleanrhapsody.kram.R;
 import com.booleanrhapsody.kram.activity.*;
 import com.booleanrhapsody.kram.adapter.DoctorsPatientsActivityMessagesRecyclerViewAdapter;
 import com.booleanrhapsody.kram.databinding.DoctorsPatientsActivityBinding;
+import com.booleanrhapsody.kram.model.PatientModel;
+
 import java.util.*;
 
 
 public class DoctorsPatientsActivity extends Fragment {
-	
+
+	private DoctorsPatientsActivityMessagesRecyclerViewAdapter mAdapter;
+
 	public static DoctorsPatientsActivity newInstance() {
 	
 		DoctorsPatientsActivity fragment = new DoctorsPatientsActivity();
@@ -50,9 +54,28 @@ public class DoctorsPatientsActivity extends Fragment {
 	}
 	
 	public void init() {
-	
+
+		this.mAdapter = new DoctorsPatientsActivityMessagesRecyclerViewAdapter(PatientModel.getPatientsQuery());
 		// Configure Messages component
 		binding.messagesRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false));
-		binding.messagesRecyclerView.setAdapter(new DoctorsPatientsActivityMessagesRecyclerViewAdapter());
+		binding.messagesRecyclerView.setAdapter(this.mAdapter);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		// Start listening for Firestore updates
+		if (mAdapter != null) {
+			mAdapter.startListening();
+		}
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		if (mAdapter != null) {
+			mAdapter.stopListening();
+		}
 	}
 }
